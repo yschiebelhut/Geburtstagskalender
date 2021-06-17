6b754251f31fb1f9fa1d3359cf28345e60402bf2
clemens1403 <Clemens.SRichter@t-online.de>
2021-06-17 15:12:34 +0200

    removed unnecessary relation

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ app.use(express.static('../frontend'))
 app.get('/', (req, res) => {
   res.set("Content-Type", "text/xml")
   var testobj = {test:"test"}
-  var xmlres = '<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE profile-screen SYSTEM "/dtd/cmd.dtd"><?xml-stylesheet type="text/xsl" href="xslt/test.xsl"?>' + xml(testobj)
+  var xmlres = '<?xml version="1.0" encoding="UTF-8"?><?xml-stylesheet type="text/xsl" href="xslt/test.xsl"?>' + xml(testobj)
   console.log(xmlres)
   res.send(xmlres)
 })
