9cd70c23d68f4144ae7b4eb0002bd1335ada62e8
clemens1403 <Clemens.SRichter@t-online.de>
2021-06-17 14:50:50 +0200

    Test files added
    XSLT not working yet

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const xml = require("xml")
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.set("Content-Type", "text/xml")
+  var testobj = {test:"test"}
+  var xmlres = '<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE profile-screen SYSTEM "/dtd/cmd.dtd"><?xml-stylesheet type="text/xsl" href="../frontend/xslt/test.xsl"?>' + xml(testobj)
+  console.log(xmlres)
+  res.send(xmlres)
 })
 
 app.listen(port, () => {
