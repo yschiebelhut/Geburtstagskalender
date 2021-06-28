85f226fa0ab94c7b60cf31c469ec47519eed58fe
Yannik Schiebelhut <yannik.schiebelhut@gmail.com>
2021-06-28 11:27:51 +0200

    added month variable and did reformatting to obey guidelines

--- backend/server.js
+++ backend/server.js
@@ -6,14 +6,18 @@ const xml = require("xml")
 
 app.use(express.static('../frontend'))
 
+const today = new Date()
+var month = today.getMonth() + 1
+console.log(month)
+
 app.get('/', (req, res) => {
-  res.set("Content-Type", "text/xml")
-  var testobj = {test:"test"}
-  var xmlres = '<?xml version="1.0" encoding="UTF-8"?><?xml-stylesheet type="text/xsl" href="xslt/test.xsl"?>' + xml(testobj)
-  console.log(xmlres)
-  res.send(xmlres)
+	res.set("Content-Type", "text/xml")
+	var testobj = { test: "test" }
+	var xmlres = '<?xml version="1.0" encoding="UTF-8"?><?xml-stylesheet type="text/xsl" href="xslt/test.xsl"?>' + xml(testobj)
+	console.log(xmlres)
+	res.send(xmlres)
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+	console.log(`Example app listening at http://localhost:${port}`)
 })
