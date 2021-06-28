510665c19d6410ed8238a6fb78400a4edeaec4b5
Yannik Schiebelhut <yannik.schiebelhut@gmail.com>
2021-06-28 15:29:22 +0200

    fucked up merging, this shall correct it

--- backend/server.js
+++ backend/server.js
@@ -5,16 +5,16 @@
 	const path = require('path')
 
 	var curMonth = new Date().getMonth() + 1
-	
+
 	const handleDBJS = require('./handleDB')
 
 	app.use(express.static('../frontend'))
-	
+
 	app.get('/', async (req, res) => {
 		res.set("Content-Type", "text/xml")
 		var convert = require('xml-js')
 		var data = await handleDBJS.getDataForMonth(curMonth)
-		var options = {compact: true, ignoreComment: true, spaces: 4};
+		var options = { compact: true, ignoreComment: true, spaces: 4 };
 		var output = ""
 		data.forEach((entry) => {
 			// does each XML doc need an root-tag? then I propose surrounding with <entries> tag
@@ -23,7 +23,7 @@
 			output += "</entry>"
 		})
 		const prettifyXml = require('prettify-xml')
-		var format = {indent: 4, newline:'\n'}
+		var format = { indent: 4, newline: '\n' }
 		output = prettifyXml(output, format)
 		var xmlres = '<?xml version="1.0" encoding="UTF-8"?><?xml-stylesheet type="text/xsl" href="xslt/test.xsl"?>' + '\n' + output
 		console.log(xmlres)
@@ -32,6 +32,10 @@
 
 	app.post("/createEntry", (req, res) => {
 		console.log(req.body)
+	})
+
+	app.get('/createEntry', (req, res) => {
+		res.sendFile(path.join(__dirname, '../frontend/html/createEntry.html'))
 	})
 
 	app.listen(port, () => {
