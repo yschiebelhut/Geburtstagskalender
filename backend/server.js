4e28197b024f5ac26e38d37f560921f2dc74d595
Yannik Schiebelhut <yannik.schiebelhut@gmail.com>
2021-06-28 15:24:43 +0200

    enabled server to make database calls and return those as XML

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,39 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
-const xml = require("xml")
-
-app.use(express.static('../frontend'))
-
-const today = new Date()
-var month = today.getMonth() + 1
-console.log(month);
-
 (async () => {
-	const handleDBJS = await require('./handleDB')
-	const result = await handleDBJS.getDataForMonth(month)
-	console.log(result)
+	const express = require('express')
+	const app = express()
+	const port = 3000
+
+	var curMonth = new Date().getMonth() + 1
+	
+	const handleDBJS = require('./handleDB')
+
+	app.use(express.static('../frontend'))
+	
+	app.get('/', async (req, res) => {
+		res.set("Content-Type", "text/xml")
+		var convert = require('xml-js')
+		var data = await handleDBJS.getDataForMonth(curMonth)
+		var options = {compact: true, ignoreComment: true, spaces: 4};
+		var output = ""
+		data.forEach((entry) => {
+			// does each XML doc need an root-tag? then I propose surrounding with <entries> tag
+			output += "<entry>"
+			output += convert.json2xml(entry, options)
+			output += "</entry>"
+		})
+		const prettifyXml = require('prettify-xml')
+		var format = {indent: 4, newline:'\n'}
+		output = prettifyXml(output, format)
+		var xmlres = '<?xml version="1.0" encoding="UTF-8"?><?xml-stylesheet type="text/xsl" href="xslt/test.xsl"?>' + '\n' + output
+		console.log(xmlres)
+		res.send(xmlres)
+	})
+
+	app.post("/createEntry", (req, res) => {
+		console.log(req.body)
+	})
+
+	app.listen(port, () => {
+		console.log(`Example app listening at http://localhost:${port}`)
+	})
 })()
-
-app.get('/', (req, res) => {
-	res.set("Content-Type", "text/xml")
-	var testobj = { test: "test" }
-	var xmlres = '<?xml version="1.0" encoding="UTF-8"?><?xml-stylesheet type="text/xsl" href="xslt/test.xsl"?>' + xml(testobj)
-	console.log(xmlres)
-	res.send(xmlres)
-})
-
-app.post("/createEntry", (req,res)=>{
-  console.log(req.body)
-})
-
-app.listen(port, () => {
-	console.log(`Example app listening at http://localhost:${port}`)
-})
