4e8d4cb1568b2d8c4f703caf58ebae45a00e7944
clemens1403 <Clemens.SRichter@t-online.de>
2021-07-01 15:27:24 +0200

    jz gehts mit den days left omegakekw

--- backend/server.js
+++ backend/server.js
@@ -38,13 +38,15 @@
 			output += '<bday>'
 
 
-			var bdDate = new Date(bdDate.setHours(0,0,0,0))
+			var bdDate = new Date()
+			bdDate.setHours(0,0,0,0)
 			bdDate.setMonth(curMonth)
 			bdDate.setDate(entry["day"])
 			if(entry["month"]<curMonth){
 				bdDate.setFullYear(bdDate.getFullYear()+1)
 			}
-			var curDate = new Date(curDate.setHours(0,0,0,0))
+			var curDate = new Date()
+			curDate.setHours(0,0,0,0)
 			var timeDiff =bdDate.getTime()-curDate.getTime()
 			var diffDays = timeDiff / (1000*3600*24)
 
