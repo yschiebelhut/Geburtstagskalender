ed8cd26d3198ee697d5b5684c1c60752d7723eaf
clemens1403 <Clemens.SRichter@t-online.de>
2021-07-01 13:34:37 +0200

    overworked createEntry.js :3

--- frontend/js/createEntry.js
+++ frontend/js/createEntry.js
@@ -1,29 +1,36 @@
 function sendData(){
   var name = document.getElementById("name").value
-  var day = document.getElementById("day").value
-  var month = document.getElementById("name").value
-  var year = document.getElementById("day").value
-  var notes = document.getElementById("name").value
+  var date = document.getElementById("date").value.split("-")
+  var day = date[2]
+  var month = date[1]
+  var year = date[0]
+  var notes = document.getElementById("notes").value
 
-  var data = {
-    name : name,
-    day : day,
-    month : month,
-    year : year,
-    notes : notes
-  }
-  data = JSON.stringify(data)
-  console.log(data)
 
-  var xhr = new XMLHttpRequest();
-  xhr.open("POST", '/createEntry', true);
 
-  xhr.setRequestHeader("Content-Type", "application/json");
-
-  xhr.onreadystatechange = function() {
-      if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-        console.log("sent data")
-      }
+  if(name && day && month && year && notes){
+    var data = {
+      name : name,
+      day : day,
+      month : month,
+      year : year,
+      notes : notes
     }
-    xhr.send(data);
+    data = JSON.stringify(data)
+    console.log(data)
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", '/createEntry', true);
+
+    xhr.setRequestHeader("Content-Type", "application/json");
+
+    xhr.onreadystatechange = function() {
+        if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+          console.log("sent data")
+        }
+      }
+      xhr.send(data);
+  }else{
+    console.log("PLS GIVE MORE INFORMATION!")
+  }
+
 }
