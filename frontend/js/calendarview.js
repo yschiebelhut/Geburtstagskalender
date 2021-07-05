427e0b16940bfbae19f581a8f0f6b4c412cb6e88
clemens1403 <Clemens.SRichter@t-online.de>
2021-07-05 21:25:02 +0200

    little big improvement
    today only shown in correct month
    but still only after request

--- frontend/js/calendarview.js
+++ frontend/js/calendarview.js
@@ -5,7 +5,7 @@ function previousMonth(){
   xhr.onreadystatechange = function() {
       if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
         console.log("-1")
-        location.reload()
+        window.location.href="/calendarview"
       }
     }
   xhr.send()
@@ -18,7 +18,7 @@ function nextMonth(){
   xhr.onreadystatechange = function() {
       if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
         console.log("+1")
-        location.reload()
+        window.location.href="/calendarview"
       }
     }
   xhr.send();
@@ -28,10 +28,11 @@ function nextMonth(){
 
 function showToday(){
   var requested = window.location.search
+  var today = new Date()
+  var day = Math.floor(today.toDateString().split(" ")[2])
+  var month=today.getMonth()
+  console.log(month)
   if(requested){
-    var today = new Date()
-    var day = Math.floor(today.toDateString().split(" ")[2])
-    console.log(day)
     document.getElementById(day).setAttribute("style", "border:2px solid red; background-color: rgb(255, 125, 115);");
   }
 
